c715d8b9ff1b779bbf768111596b9294af8556b5
Joshua Kagawa <joshuakagawa@gmail.com>
2021-09-11 16:15:11 -0400

    Polish UI elements

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ db = SQLAlchemy()
 #database_path = "postgres://{}:{}@{}/{}".format('postgres', 'emerald', 'localhost:5432', database_name)
 #database_path = os.environ['DATABASE_URL']
 database_name = "d8b3a9n3e5jv3p"
-database_path = "postgres://{}:{}@{}/{}".format('esvqiibikborvn', '9f493fabfcbd0dbdd41ffd093121def61262c48b3622f1a9dd0889cc5e3270ba', 'ec2-18-214-211-47.compute-1.amazonaws.com:5432', database_name)
+database_path = "postgresql://{}:{}@{}/{}".format('esvqiibikborvn', '9f493fabfcbd0dbdd41ffd093121def61262c48b3622f1a9dd0889cc5e3270ba', 'ec2-18-214-211-47.compute-1.amazonaws.com:5432', database_name)
 
 def setup_db(app):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
@@ -20,7 +20,7 @@ def setup_db(app):
 
 def return_db():
     return db
-    
+
 def db_create_all():
     db.create_all()
 
@@ -36,14 +36,14 @@ class ClimbingSpot(db.Model):
     indoor_or_outdoor = db.Column(db.Integer)
     outdoor_coordinates = db.Column(db.String)
     visited_spot = db.relationship('VisitedSpot', backref='climbingspot', lazy=True)
-    
+
 class Climber(db.Model):
     __tablename__ = 'climber'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     state = db.Column(db.String)
     visited_spot = db.relationship('VisitedSpot', backref='climber', lazy=True)
-    
+
 class VisitedSpot(db.Model):
     __tablename__ = 'visitedspot'
     id = db.Column(db.Integer, primary_key=True)
